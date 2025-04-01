b4b74308bd9965270dfcfe1006da4bc0b85c97ea
mac <iommy_dev@mail.ru>
2025-04-01 23:54:04 +0300

    feat: implement bpmn-site

---
+++ src/pages/main/main.jsx
@@ -0,0 +1,23 @@
+import s from "./main.module.css"
+import vector_circle from "../../../public/assets/vectors/vector.png"
+
+export function Main() {
+    return (
+        <>
+        <div className={s.wrapper_v1}>
+            <div>
+                <img className={s.vector} src={vector_circle}></img>
+
+                <div>
+
+                </div>
+            </div>
+        </div>
+
+            <div>
+
+            </div>
+
+        </>
+    )
+}
