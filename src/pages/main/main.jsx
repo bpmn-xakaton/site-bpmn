734aa5103605fe120c4bc87df72b03ee3d3d229e
mac <iommy_dev@mail.ru>
2025-04-02 00:49:48 +0300

    feat: add person and vector

--- src/pages/main/main.jsx
+++ src/pages/main/main.jsx
@@ -1,18 +1,21 @@
 import s from "./main.module.css"
 import vector_circle from "../../../public/assets/vectors/vector.png"
+import person from "../../../public/assets/vectors/person.png"
 
 export function Main() {
     return (
         <>
-        <div className={s.wrapper_v1}>
-            <div>
-                <img className={s.vector} src={vector_circle}></img>
-
-                <div>
+            <div className={s.wrapper_v1}>
+                <div className={s.wrapper_bottom}>
+                    <img className={s.vector} src={vector_circle}></img>
 
                 </div>
+
+                <div className={s.wrapper_v2}>
+                    <img className={s.person} src={person}></img>
+                </div>
+
             </div>
-        </div>
 
             <div>
 
